981ba73b70159adcc5a7ab224335c12fe2356d91
Alexander F. Rødseth <rodseth@gmail.com>
2018-09-06 13:17:04 +0200

    Various

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"encoding/json"
 )
+
+type Configuration struct {
+	AliasExpressions []string
+}
 
 // interpret an alias expression on the form: alias=username@hostname:port
 func interpret(aliasExpression string) (string, string, string, string) {
@@ -21,7 +26,44 @@ func interpret(aliasExpression string) (string, string, string, string) {
 	return alias, username, hostname, port
 }
 
+func ReadConfig(filename string) (*Configuration, error) {
+    file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	config := &Configuration{}
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func WriteConfig(filename string, config *Configuration) error {
+	file, err := os.OpenFile(filename, os.O_CREATE | os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	conf, err := ReadConfig("~/.config/copy.conf")
+	if err != nil {
+		err = WriteConfig("~/.config/copy.conf", &Configuration{})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+
 	var addHost, removeHost string
 	flag.StringVar(&addHost, "a", "", "Add a host alias")
 	flag.StringVar(&removeHost, "r", "", "Remove a host alias")
@@ -29,10 +71,16 @@ func main() {
 
 	if addHost != "" {
 		if !(strings.Contains(addHost, "=") && strings.Contains(addHost, "@") && strings.Contains(addHost, ":")) {
-			fmt.Println("INVALID ALIAS EXPRESSION", addHost)
+			fmt.Fprintln(os.Stderr, "INVALID ALIAS EXPRESSION", addHost)
 			os.Exit(1)
 		}
 		alias, username, hostname, port := interpret(addHost)
+		conf.AliasExpressions = append(conf.AliasExpressions, addHost)
+		err = WriteConfig("~/.config/copy.conf", conf)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		fmt.Printf("SAVED %s@%s:%s as %s\n", username, hostname, port, alias)
 		os.Exit(0)
 	}
@@ -44,11 +92,11 @@ func main() {
 
 	fmt.Println(flag.Args())
 	if len(flag.Args()) < 2 {
-		fmt.Println("TWO ARGUMENTS ARE REQUIRED")
+		fmt.Fprintln(os.Stderr, "TWO ARGUMENTS ARE REQUIRED")
 		os.Exit(1)
 	}
 	from := flag.Args()[0]
 	to := flag.Args()[1]
 
-	fmt.Println("COYP", from, to)
+	fmt.Println("COPY", from, to)
 }
