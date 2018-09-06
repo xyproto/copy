135e1b49e7b546042280c387a8f993cebd4947f9
Alexander F Rødseth <rodseth@gmail.com>
2018-09-06 09:03:39 -0700

    Add a working alias system

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # copy
 
-# WORK IN PROGRESS, early stages
+A better copy command, with a progress bar, concurrent copying and the ability to use host aliases for username+host+port when coping over ssh.
 
-Copy a file locally or over ssh, and ask before overwriting. `pscp` does not ask before overwriting.
+Should always ask before overwriting files. `pscp` can not ask before overwriting.
 
-* Use `-a` to add a username@hostname:port alias on the form: `alias=username@hostname:port`
+# THIS IS A WORK IN PROGRESS AND NOT NEARLY DONE
+
+* Use `-a` to add a username@hostname:port alias on the form: `alias=username@hostname:port`.
 * Use `-r` to remove a remote host alias.
+* Use `-l` to list host aliases.
 
 Takes two argument, the file to copy from and the file to copy to.
-
-If the filename starts with an exclamation mark, it is interpreted as a remote host alias instead.
--- main.go
+++ main.go
@@ -1,33 +1,75 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
-	"encoding/json"
 )
 
+type AliasExpression struct {
+	Alias    string `alias`
+	Username string `username`
+	Hostname string `hostname`
+	Port     int    `port`
+}
+
+var configFile = filepath.Join(os.Getenv("HOME"), ".config/copy.conf")
+
 type Configuration struct {
-	AliasExpressions []string
+	RemoteHosts []AliasExpression
 }
 
 // interpret an alias expression on the form: alias=username@hostname:port
-func interpret(aliasExpression string) (string, string, string, string) {
-	fields := strings.SplitN(aliasExpression, "=", 2)
+func NewAliasExpression(unparsedAliasExpression string) (*AliasExpression, error) {
+	if !strings.Contains(unparsedAliasExpression, "=") {
+		return nil, errors.New("Alias expressions must contain an equal sign!")
+	}
+	fields := strings.SplitN(unparsedAliasExpression, "=", 2)
 	alias := fields[0]
 	usernameHostPort := fields[1]
-	fields = strings.SplitN(usernameHostPort, "@", 2)
-	username := fields[0]
-	hostPort := fields[1]
-	fields = strings.SplitN(hostPort, ":", 2)
-	hostname := fields[0]
-	port := fields[1]
-	return alias, username, hostname, port
+	var (
+		username string
+		hostPort string
+	)
+	if !strings.Contains(usernameHostPort, "@") {
+		username = os.Getenv("LOGNAME")
+		hostPort = usernameHostPort
+	} else {
+		fields = strings.SplitN(usernameHostPort, "@", 2)
+		username = fields[0]
+		hostPort = fields[1]
+	}
+	var (
+		hostname string
+		port     int
+		err      error
+	)
+	if !strings.Contains(hostPort, ":") {
+		hostname = hostPort
+		port = 22
+	} else {
+		fields = strings.SplitN(hostPort, ":", 2)
+		hostname = fields[0]
+		port, err = strconv.Atoi(fields[1])
+		if err != nil {
+			port = 22
+		}
+	}
+	ae := &AliasExpression{}
+	ae.Alias = alias
+	ae.Username = username
+	ae.Hostname = hostname
+	ae.Port = port
+	return ae, nil
 }
 
 func ReadConfig(filename string) (*Configuration, error) {
-    file, err := os.Open(filename)
+	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +84,7 @@ func ReadConfig(filename string) (*Configuration, error) {
 }
 
 func WriteConfig(filename string, config *Configuration) error {
-	file, err := os.OpenFile(filename, os.O_CREATE | os.O_WRONLY, 0644)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -55,48 +97,115 @@ func WriteConfig(filename string, config *Configuration) error {
 	return nil
 }
 
+// Check if the given list of strings has the given string
+func has(sl []string, s string) bool {
+	for _, e := range sl {
+		if e == s {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if the given list of aliasExpressions contains the given alias
+func ahas(aes []AliasExpression, ae *AliasExpression) bool {
+	for _, e := range aes {
+		if e.Alias == ae.Alias {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	conf, err := ReadConfig("~/.config/copy.conf")
+	conf, err := ReadConfig(configFile)
 	if err != nil {
-		err = WriteConfig("~/.config/copy.conf", &Configuration{})
+		conf = &Configuration{}
+		err = WriteConfig(configFile, conf)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 	}
 
-	var addHost, removeHost string
-	flag.StringVar(&addHost, "a", "", "Add a host alias")
+	var (
+		unparsedAliasExpression, removeHost string
+		listHosts                           bool
+	)
+	flag.StringVar(&unparsedAliasExpression, "a", "", "Add a host alias")
 	flag.StringVar(&removeHost, "r", "", "Remove a host alias")
+	flag.BoolVar(&listHosts, "l", false, "List host aliases")
 	flag.Parse()
 
-	if addHost != "" {
-		if !(strings.Contains(addHost, "=") && strings.Contains(addHost, "@") && strings.Contains(addHost, ":")) {
-			fmt.Fprintln(os.Stderr, "INVALID ALIAS EXPRESSION", addHost)
-			os.Exit(1)
-		}
-		alias, username, hostname, port := interpret(addHost)
-		conf.AliasExpressions = append(conf.AliasExpressions, addHost)
-		err = WriteConfig("~/.config/copy.conf", conf)
+	if listHosts {
+		fmt.Println(conf.RemoteHosts)
+		os.Exit(0)
+	}
+
+	if unparsedAliasExpression != "" {
+		newAE, err := NewAliasExpression(unparsedAliasExpression)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		fmt.Printf("SAVED %s@%s:%s as %s\n", username, hostname, port, alias)
+		if ahas(conf.RemoteHosts, newAE) {
+			var newRemoteHosts []AliasExpression
+			for _, aliasExpression := range conf.RemoteHosts {
+				if aliasExpression.Alias == newAE.Alias {
+					// Already exists
+					newRemoteHosts = append(newRemoteHosts, *newAE)
+				} else {
+					newRemoteHosts = append(newRemoteHosts, aliasExpression)
+				}
+			}
+			conf.RemoteHosts = newRemoteHosts
+		} else {
+			conf.RemoteHosts = append(conf.RemoteHosts, *newAE)
+		}
+		err = WriteConfig(configFile, conf)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		//fmt.Printf("SAVED %s@%s:%d as %s\n", newAE.Username, newAE.Hostname, newAE.Port, newAE.Alias)
 		os.Exit(0)
 	}
 
 	if removeHost != "" {
-		fmt.Printf("REMOVED %s\n", removeHost)
+		var filteredRemoteHosts []AliasExpression
+		for _, aliasExpression := range conf.RemoteHosts {
+			if aliasExpression.Alias == removeHost {
+				//fmt.Printf("REMOVED %s\n", removeHost)
+				// Skip
+				continue
+			}
+			filteredRemoteHosts = append(filteredRemoteHosts, aliasExpression)
+		}
+		conf.RemoteHosts = filteredRemoteHosts
+		err = WriteConfig(configFile, conf)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		os.Exit(0)
 	}
 
-	fmt.Println(flag.Args())
 	if len(flag.Args()) < 2 {
-		fmt.Fprintln(os.Stderr, "TWO ARGUMENTS ARE REQUIRED")
+		fmt.Fprintln(os.Stderr, "Two arguments are required:")
+		fmt.Fprintln(os.Stderr, "copy FROM TO")
 		os.Exit(1)
 	}
+
 	from := flag.Args()[0]
 	to := flag.Args()[1]
 
-	fmt.Println("COPY", from, to)
+	// First assume that from and to are local files.
+	// Look into aliases only if local files of the same names are not found.
+
+	a, b := os.Stat(from)
+	c, d := os.Stat(to)
+
+	fmt.Println(a, b, c, d)
+
+	fmt.Println("TO IMPLEMENT: COPY", from, to)
 }
